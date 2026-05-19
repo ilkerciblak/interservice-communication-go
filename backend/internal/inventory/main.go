47732b7ac628cf836b60f106c7bbfb32b13c1a1b
ilkerciblak <ilkerciblak@gmail.com>
2026-05-19 15:47:43 +0300

    feat: implement eventbus and subscribers on inventory service

--- backend/internal/inventory/main.go
+++ backend/internal/inventory/main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	eventbus "ilkerciblak/order-management/internal/event-bus"
+	"ilkerciblak/order-management/shared/messaging"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"google.golang.org/grpc"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+	defer stop()
+	eventBus, err := messaging.RegisterRabbitMQ()
+	if err != nil {
+		log.Fatalf("failed to initialize rabbitMQ: %v", err)
+	}
+	defer eventBus.Close(ctx)
 
 	repo := inventoryRepository{}
 	service := inventoryService{Repo: &repo}
+
+	if err := eventBus.Subscribe(ctx, eventbus.OrderPlaced, func(ctx context.Context, e messaging.Event) error {
+		var payload eventbus.OrderPlacedPayload
+		if err := json.Unmarshal(e.Payload, &payload); err != nil {
+			log.Printf("[inventory] failed to decode event %s payload: %v", e.Name, e.Payload)
+			return err
+		}
+
+		log.Printf("[inventory] event %s | ", e.Name)
+		var res = eventbus.StockReservedPayload{
+			OrderID:  payload.OrderID,
+			Item:     payload.Item,
+			Quantity: payload.Quantity,
+			Reserved: true,
+		}
+		if err := service.ReserveProduct(ctx, payload.Item); err != nil {
+			res = eventbus.StockReservedPayload{Reserved: false, OrderID: payload.OrderID, Item: payload.Item, Quantity: payload.Quantity, Message: err.Error()}
+		}
+
+		return eventBus.Publish(ctx, eventbus.StockReservedEvent(res))
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("[inventory] subscribed to `order.placed` publishes `stock.reserved`")
 
 	grpcServer := grpc.NewServer()
 	initInventoryServer(grpcServer, &service)
@@ -20,6 +59,22 @@ func main() {
 		log.Fatalf("failed to start inventory server: %v", err)
 	}
 
-	log.Fatal(grpcServer.Serve(ls))
+	go func() {
+		if err := eventBus.Start(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		if err := grpcServer.Serve(ls); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		grpcServer.GracefulStop()
+		os.Exit(1)
+	}
 
 }
