fac2df8404937a114766e0634744663bf6bba42c
ilkerciblak <ilkerciblak@gmail.com>
2026-05-19 15:19:42 +0300

    feat: implement event publisher and subscribes

--- backend/internal/order-management/service.go
+++ backend/internal/order-management/service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"crypto/rand"
 	"fmt"
+	eventbus "ilkerciblak/order-management/internal/event-bus"
+	"ilkerciblak/order-management/shared/messaging"
 	inventorypb "ilkerciblak/order-management/shared/proto/inventory"
 	notificationpb "ilkerciblak/order-management/shared/proto/notification"
 	"log"
@@ -14,6 +16,7 @@ type OrderService struct {
 	Repository         OrderRepositoryInterface
 	inventoryClient    inventorypb.InventoryServiceClient
 	notificationClient notificationpb.NotificationServiceClient
+	messaging.Publisher
 }
 
 func (s *OrderService) PlaceOrder(ctx context.Context, customer, item string, quantity int) (*Order, error) {
@@ -40,12 +43,22 @@ func (s *OrderService) PlaceOrder(ctx context.Context, customer, item string, qu
 	if err := s.Repository.CreateOrder(ctx, order); err != nil {
 		return nil, err
 	}
+	// replaced with event publisher
+	//
+	// if _, err := s.notificationClient.SendConfirmation(ctx, &notificationpb.SendConfirmationRequest{
+	// 	Customer: order.Customer,
+	// 	OrderId:  order.ID,
+	// }); err != nil {
+	// 	return nil, fmt.Errorf("notification failed: %w", err)
+	// }
 
-	if _, err := s.notificationClient.SendConfirmation(ctx, &notificationpb.SendConfirmationRequest{
+	if err := s.Publish(ctx, eventbus.OrderPlacedEvent(eventbus.OrderPlacedPayload{
+		OrderID:  order.ID,
 		Customer: order.Customer,
-		OrderId:  order.ID,
-	}); err != nil {
-		return nil, fmt.Errorf("notification failed: %w", err)
+		Item:     order.Item,
+		Quantity: int32(order.Quantity),
+	})); err != nil {
+		log.Println("[order] failed to publish order.placed: %v", err)
 	}
 
 	log.Printf("[order] placed %s for %s (%dx %s)\n", order.ID, order.Customer, order.Quantity, order.Item)
